8101af0a15d096f63df32c21b87af7eb9146b042
Nick Sprague <n.sprague18@gmail.com>
2024-03-02 18:27:48 -0500

    added starter HTML, working on show button with Vue.js, added background image and slight styling with css

--- src/app.vue
+++ src/app.vue
@@ -1,1 +1,19 @@
-console.log('Test Vue')
+// console.log('Test Vue')
+
+const app = Vue.createApp({
+    data() {
+        return {
+            showWeather: false,
+            weather1: 'Test String',
+            weather2: 'Second Test',
+            weather3: 3,
+        }
+    },
+    methods: {
+        toggleShowWeather() {
+            this.showWeather = !this.showWeather
+        }
+    }
+})
+
+app.mount('#app')
